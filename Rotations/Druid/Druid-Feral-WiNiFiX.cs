9b0f1961d8537c2b5697d5c097a941efdc0c1d21
WiNiFiX <30388134+winifix@users.noreply.github.com>
2017-10-23 17:39:14 +0200

    Update Feral to correct rotation system

--- Rotations/Druid/Druid-Feral-WiNiFiX.cs
+++ Rotations/Druid/Druid-Feral-WiNiFiX.cs
@@ -30,45 +30,101 @@ namespace Frozen.Rotation
             var RipTime = WoW.TargetDebuffTimeRemaining("Rip");
             var MoonfireTime = WoW.TargetDebuffTimeRemaining("Moonfire");
             
-            WoW.CastSpell("Berserk", UseCooldowns && !WoW.PlayerHasBuff("Prowl"));
+			if (UseCooldowns && !WoW.PlayerHasBuff("Prowl") && WoW.CanCast("Berserk"))
+			{
+				WoW.CastSpell("Berserk"); // No return needed off the GCD				
+			}
 
-            WoW.CastSpell("Regrowth", WoW.PlayerHasBuff("PredatorySwiftness") && WoW.PlayerBuffStacks("Bloodtalons") != 2 && !WoW.PlayerHasBuff("Prowl"));
+			if (WoW.PlayerHasBuff("PredatorySwiftness") && WoW.PlayerBuffStacks("Bloodtalons") != 2 && !WoW.PlayerHasBuff("Prowl") && WoW.CanCast("Regrowth"))
+			{
+				WoW.CastSpell("Regrowth");
+				return;
+			}
 
             // Cast Ferocious Bite if at 5 Combo Points and Rip / Savage Roar do not need refreshing within 10 sec.
-            WoW.CastSpell("FerociousBite", WoW.CurrentComboPoints >= 5 && RipTime > 1000 && SavageRoarTime > 1000 && !WoW.PlayerHasBuff("Prowl"));
+			
+			if (WoW.CurrentComboPoints >= 5 && RipTime > 1000 && SavageRoarTime > 1000 && !WoW.PlayerHasBuff("Prowl") && WoW.CanCast("FerociousBite"))
+			{
+				WoW.CastSpell("FerociousBite");
+				return;
+			}
 
             // Maintain Savage Roar if taken.
-            WoW.CastSpell("SavageRoar", WoW.Talent(6) == 3 && SavageRoarTime <= 200 && WoW.CurrentComboPoints >= 1 && WoW.Energy >= 40 && !WoW.PlayerHasBuff("Prowl"));
+			if (WoW.Talent(6) == 3 && SavageRoarTime <= 200 && WoW.CurrentComboPoints >= 1 && WoW.Energy >= 40 && !WoW.PlayerHasBuff("Prowl") && WoW.CanCast("SavageRoar"))
+			{
+				WoW.CastSpell("SavageRoar");
+				return;
+			}
 
             // Maintain Rake.
-            WoW.CastSpell("Rake", RakeTime <= 200 && WoW.Energy >= 35);
+			if (RakeTime <= 200 && WoW.Energy >= 35 && WoW.CanCast("Rake"))
+			{
+				WoW.CastSpell("Rake");
+				return;
+			}
 
             // Maintain Rip (at below 25%, or with Sabertooth taken, replace with Ferocious Bite).
-            WoW.CastSpell("Rip", RipTime <= 200 && WoW.Talent(6) != 1 && WoW.CurrentComboPoints >= 1 && WoW.Energy >= 30);
-            WoW.CastSpell("FerociousBite", RipTime <= 200 && WoW.TargetHealthPercent <= 25 && WoW.Talent(6) == 1 && WoW.CurrentComboPoints >= 1);
+			if (RipTime <= 200 && WoW.Talent(6) != 1 && WoW.CurrentComboPoints >= 1 && WoW.Energy >= 30 && WoW.CanCast("Rip"))
+			{
+				WoW.CastSpell("Rip");
+				return;
+			}
+			if (RipTime <= 200 && WoW.TargetHealthPercent <= 25 && WoW.Talent(6) == 1 && WoW.CurrentComboPoints >= 1 && WoW.CanCast("FerociousBite"))
+			{
+				WoW.CastSpell("FerociousBite", );
+				return;
+			}
 
             // Maintain Moonfire if Lunar Inspiration is taken.
-            WoW.CastSpell("Moonfire", MoonfireTime <= 200 && WoW.Energy >= 30 && WoW.Talent(1) == 3);
+			if (MoonfireTime <= 200 && WoW.Energy >= 30 && WoW.Talent(1) == 3 && WoW.CanCast("Moonfire"))
+			{
+				WoW.CastSpell("Moonfire");
+				return;
+			}
 
             // Use any Omen of Clarity procs to maintain Thrash if using Luffa Wrappings.
             // Will code this when i get the item till then stuff it.... :)
 
             // Cast Tiger's Fury at 20 Energy or less.
-            WoW.CastSpell("TigersFury", WoW.Energy <= 20, false);
+			if (WoW.Energy <= 20 && WoW.CanCast("TigersFury"))
+			{
+				WoW.CastSpell("TigersFury"); // Off the GCD no return needed				
+			}
 
             // Cast Ashamane's Frenzy, try to sync this with Tiger's Fury uses.
-            WoW.CastSpell("Ashamane", true);
+			if (WoW.CanCast("Ashamane"))
+			{
+				WoW.CastSpell("Ashamane");
+				return;
+			}
             
             // Cast Shred to build combo points.
-            WoW.CastSpell("Shred", (WoW.Energy >= 40 || WoW.PlayerHasBuff("ClearCasting")) && WoW.CountEnemyNPCsInRange <= 1);
-            WoW.CastSpell("Thrash", (WoW.Energy >= 40 || WoW.PlayerHasBuff("ClearCasting")) && WoW.CountEnemyNPCsInRange >= 2);
+			if ((WoW.Energy >= 40 || WoW.PlayerHasBuff("ClearCasting")) && WoW.CountEnemyNPCsInRange <= 1 && WoW.CanCast("Shred"))
+			{
+				WoW.CastSpell("Shred");
+				return;
+			}
+			if ((WoW.Energy >= 40 || WoW.PlayerHasBuff("ClearCasting")) && WoW.CountEnemyNPCsInRange >= 2 && WoW.CanCast("Thrash"))
+			{
+				WoW.CastSpell("Thrash");
+				return;
+			}
 
             // When using Brutal Slash, try to make sure that you always have at least one charge on cooldown, 
             // but have as many charges as possible when a wave of enemies comes into range. 
             // Try to have short duration buffs such as Tiger's Fury and Bloodtalons active 
             // on as many high target casts of this as possible, as it deals massive AoE burst damage with each cast.
-            WoW.CastSpell("BrutalSlash", WoW.Talent(6) == 2 && WoW.Energy >= 20 && WoW.PlayerSpellCharges("BrutalSlash") > 1);
-            WoW.CastSpell("BrutalSlash", WoW.Talent(6) == 2 && WoW.Energy >= 20 && WoW.PlayerSpellCharges("BrutalSlash") == 1 && WoW.PlayerHasBuff("TigersFury"));
+			if (WoW.Talent(6) == 2 && WoW.Energy >= 20 && WoW.PlayerSpellCharges("BrutalSlash") > 1 && WoW.CanCast("BrutalSlash"))
+			{
+				WoW.CastSpell("BrutalSlash");
+				return;
+			}
+		
+			if (WoW.Talent(6) == 2 && WoW.Energy >= 20 && WoW.PlayerSpellCharges("BrutalSlash") == 1 && WoW.PlayerHasBuff("TigersFury") && WoW.CanCast("BrutalSlash"))
+			{
+				WoW.CastSpell("BrutalSlash");
+				return;
+			}
         }
 
         public override Form SettingsForm { get; set; }
